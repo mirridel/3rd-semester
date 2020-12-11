96ed6719255dc4eb2902841e799df61ffb8bb1ec
mirridel <71335429+mirridel@users.noreply.github.com>
2020-12-11 18:36:12 +0700

    v0.02

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 ﻿#include "list.h"
 #include "matrix.h"
 #include "rectangularMatrix.h"
+#include "test.hpp"
 #include <iostream>
 
 using namespace std;
+
+void test();
 
 void main()
 {
@@ -13,6 +16,8 @@ void main()
     Matrix m1(2), m2(2), m3(2);
 
     RectangularMatrix M(2, 3);
+
+    test();
 
     p = &m1;
     cout << p->ToString() << endl;
@@ -40,4 +45,23 @@ void main()
 
     // Печать элемента по логическому номеру
     L.Print(1);
+}
+
+void test()
+{
+    if (test1())
+        cout << "Test passed #1 (+)" << endl;
+    else cout << "Test failed #1 (+)" << endl;
+
+    if (test2())
+        cout << "Test passed #2 (-)" << endl;
+    else cout << "Test failed #2 (-)" << endl;
+
+    if (test3())
+        cout << "Test passed #3 (*)" << endl;
+    else cout << "Test failed #3 (*)" << endl;
+
+    if (test4())
+        cout << "Test passed #4 (=)" << endl;
+    else cout << "Test failed #4 (=)" << endl;
 }
