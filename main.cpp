02ee0805ecfea87ccefe07e864cd2882eb162373
mirridel <71335429+mirridel@users.noreply.github.com>
2020-12-25 17:39:53 +0700

    v0.00

---
+++ main.cpp
@@ -0,0 +1,144 @@
+﻿#include <iostream>
+#include <stack>
+#include <set>
+#include <ctime>
+#include <iterator>
+#include "matrix.h"
+using namespace std;
+
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    unsigned start_t, end_t;
+    unsigned int n = 1024;
+
+    Matrix* p;
+    p = new Matrix[n];
+
+    //----------------------------------------------------------//
+
+    stack <Matrix> matrixStack;
+
+    cout << "--------------- Стек с матрицами(matrix) ---------------" << endl;
+
+    start_t = clock();
+    cout << "Добавление случайных значений(matrix) в стек " << n << endl;
+    for (int i = 0; i < n; i++) {
+        matrixStack.push(p[i]);
+    }
+    end_t = clock() - start_t;
+    cout << "Время добавление в стек: " << end_t << endl << endl;
+
+    cout << "Удаление последнего элемента(matirx) в стеке" << endl;
+    start_t = clock();
+    matrixStack.pop();
+    end_t = clock() - start_t;
+    cout << "Время удаления элемента: " << end_t << endl << endl;
+
+    //----------------------------------------------------------//
+
+    stack <int> intStack;
+
+    cout << "--------------- Стек с числами(int) ---------------" << endl;
+
+    start_t = clock();
+    cout << "Добавление случайных значений(int) в стек " << n << endl;
+    for (int i = 0; i < n; i++) {
+        int random = rand() % 10 + 1;
+        intStack.push(random);
+    }
+    end_t = clock() - start_t;
+    cout << "Время добавление в стек: " << end_t << endl << endl;
+
+    cout << "Удаление последнего элемента в стеке" << endl;
+    start_t = clock();
+    intStack.pop();
+    end_t = clock() - start_t;
+    cout << "Время удаления элемента: " << end_t << endl << endl;
+
+    //----------------------------------------------------------//
+
+    multiset <int> Multiset;
+    multiset <int> ::iterator it1;
+
+    cout << "--------------- Мультимножество с числами(int) ---------------" << endl;
+
+    start_t = clock();
+    cout << "Добавление случайных значений " << n <<endl;
+    for (int i = 0; i < n; i++) {
+        int random = rand() % 10 + 1;
+        Multiset.insert(random);
+    }
+    end_t = clock() - start_t;
+    cout << "Время добавление элементов: " << end_t << endl << endl;
+
+    cout << "Удаление всех элементов с ключом '10'" << endl;
+    start_t = clock();
+    Multiset.erase(10);
+    end_t = clock() - start_t;
+    cout << "Время удаления элементов: " << end_t << endl << endl;
+
+    cout << "Поиск элемента с ключом '5'" << endl;
+    start_t = clock();
+    it1 = Multiset.find(5);
+    end_t = clock() - start_t;
+    cout << "Время поиска элемента с ключом '5': " << end_t << endl << endl;
+
+    //----------------------------------------------------------//
+
+    multiset <float> floatMultiset;
+    multiset <float> ::iterator it2;
+
+    cout << "--------------- Мультимножество с числами(float) ---------------" << endl;
+
+    start_t = clock();
+    cout << "Добавление случайных значений " << n << endl;
+    for (int i = 0; i < n; i++) {
+        int random = rand() % 10 + 1;
+        floatMultiset.insert(random);
+    }
+    end_t = clock() - start_t;
+    cout << "Время добавление элементов: " << end_t << endl << endl;
+
+    cout << "Удаление всех элементов с ключом '10'" << endl;
+    start_t = clock();
+    floatMultiset.erase(10);
+    end_t = clock() - start_t;
+    cout << "Время удаления элементов: " << end_t << endl << endl;
+
+    cout << "Поиск элемента с ключом '5'" << endl;
+    start_t = clock();
+    it2 = floatMultiset.find(5);
+    end_t = clock() - start_t;
+    cout << "Время поиска элемента с ключом '5': " << end_t << endl << endl;
+
+    //----------------------------------------------------------//
+
+    start_t = clock();
+    while (!matrixStack.empty()) {
+        matrixStack.pop();
+    }
+    end_t = clock() - start_t;
+    cout << "Удаление стека с матрицами: " << end_t << endl;
+
+    start_t = clock();
+    while (!intStack.empty()) {
+        intStack.pop();
+    }
+    end_t = clock() - start_t;
+    cout << "Удаление стека с числами: " << end_t << endl;
+
+    start_t = clock();
+    Multiset.clear();
+    end_t = clock() - start_t;
+    cout << "Удаление мультимножества c числами: " << end_t << endl;
+
+    start_t = clock();
+    floatMultiset.clear();
+    end_t = clock() - start_t;
+    cout << "Удаление мультимножества c вещественными числами: " << end_t << endl;
+
+    system("pause");
+    return 0;
+}
---
+++ matrix.cpp
@@ -0,0 +1,198 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "matrix.h"
+
+Matrix::Matrix()
+{
+	this->size = 3;
+
+	matrix = new int* [size];
+	for (int i = 0; i < size; i++)
+		matrix[i] = new int[size];
+
+	for (int i = 0; i < size; i++)
+		for (int j = 0; j < size; j++)
+			matrix[i][j] = rand() % 10;
+}
+
+Matrix::Matrix(int size)
+{
+	if (size > 0)
+	{
+		this->size = size;
+
+		matrix = new int* [size];
+		for (int i = 0; i < size; i++)
+			matrix[i] = new int[size];
+
+		for (int i = 0; i < size; i++)
+			for (int j = 0; j < size; j++)
+				matrix[i][j] = rand() % 10;
+	}
+}
+
+	Matrix::Matrix(const Matrix &other)
+	{
+		if ( other.size > 0 )
+		{
+			this->size = other.size;
+
+			this->matrix = new int* [size];
+			for (int i = 0; i < size; i++)
+				matrix[i] = new int[size];
+
+			for (int i = 0; i < size; i++)
+				for (int j = 0; j < size; j++)
+					matrix[i][j] = other.matrix[i][j];
+		}
+	}
+
+	std::ostream& operator << (std::ostream& os, Matrix& cc)
+	{
+		for (int i = 0; i < cc.size; i++)
+		{
+			for (int j = 0; j < cc.size; j++)
+				os << cc.matrix[i][j] << "\t";
+			os << endl;
+		}
+		return os;
+	};
+
+	std::fstream& operator << (std::fstream& ofs, Matrix& cc) {
+
+		ofs.clear();
+
+		ofs.seekg(0, std::ios_base::beg);
+
+		for (int i = 0; i < cc.size; i++)
+		{
+			for (int j = 0; j < cc.size; j++)
+				ofs << cc.matrix[i][j] << "\t";
+			ofs << endl;
+		}
+		ofs << endl;
+		return ofs;
+	};
+
+	std::istream& operator >> (std::istream& is, Matrix& cc)
+	{
+		for (int i = 0; i < cc.size; i++)
+			for (int j = 0; j < cc.size; j++)
+				is >> cc.matrix[i][j];
+		return is;
+	}
+
+	std::fstream& operator >> (std::fstream& ifs, Matrix& cc)
+	{
+		ifs.seekg(0, std::ios_base::beg);
+
+		for (int i = 0; i < cc.size; i++)
+			for (int j = 0; j < cc.size; j++)
+				ifs >> cc.matrix[i][j];
+		return ifs;
+	}
+
+	char* Matrix::toString()
+	{
+		if (this->size != 0)
+		{
+			char* buff = new char [128];
+
+			buff[0] = '\0';
+
+			for (int i = 0; i < size; i++)
+			{
+				for (int j = 0; j < size; j++)
+				{
+					char subBuff[16] = "";
+					_itoa(matrix[i][j], subBuff, 10);
+					strcat(subBuff, "\t");
+					strcat(buff, subBuff);
+				}
+				strcat(buff, "\n");
+			}
+
+			return buff;
+		}
+	}
+
+	bool Matrix::SetData(int x, int y, int data)
+	{
+		std::cout << "A["<< x <<","<< y <<"] = "<< data << std::endl;
+
+		if (( x > 0 && x < size) && ( y > 0 && y < size ))
+			this->matrix[x][y] = data;
+
+		return true;
+	}
+
+	int Matrix::GetData(int x, int y)
+	{
+		int data = 0;
+		if ((x > 0 && x < size) && (y > 0 && y < size))
+			data = this->matrix[x][y];
+
+		return data;
+	}
+
+	bool Matrix::TransposeMatrix(Matrix &other)
+	{
+		if (other.size > 0)
+		{
+			int size = other.size;
+
+			int** subMatrix = new int* [size];
+			for (int i = 0; i < size; i++)
+				subMatrix[i] = new int[size];
+
+			for (int j = 0; j < size; j++)
+				for (int i = 0; i < size; i++)
+					subMatrix[i][j] = other.matrix[j][i];
+
+			for (int i = 0; i < size; i++)
+				for (int j = 0; j < size; j++)
+					other.matrix[i][j] = subMatrix[i][j];
+
+			for (int i = 0; i < size; i++)
+				delete[] subMatrix[i];
+			delete[] subMatrix;
+
+			return true;
+		}
+		else return false;
+	}
+
+	bool Matrix::AdditionMatrix(Matrix &other)
+	{
+		if (this->size == other.size && (other.size > 0 && this->size > 0))
+		{
+			for (int i = 0; i < size; i++)
+				for (int j = 0; j < size; j++)
+					this->matrix[i][j] += other.matrix[i][j];
+
+			return true;
+		}
+		else return false;
+	}
+
+	bool Matrix::SubstractionMatrix(Matrix &other)
+	{
+		if (this->size == other.size && (other.size > 0 && this->size > 0))
+		{
+			for (int i = 0; i < size; i++)
+				for (int j = 0; j < size; j++)
+					this->matrix[i][j] -= other.matrix[i][j];
+
+			return true;
+		}
+		else return false;
+	}
+
+	Matrix::~Matrix()
+	{
+		for (int i = 0; i < size; i++)
+		{
+			delete[] this->matrix[i];
+		}
+		delete[] this->matrix;
+	}
